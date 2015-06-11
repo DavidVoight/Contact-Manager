6dbb7a52213269e0779fe837110bb3d295e64b77
DavidVoight <David.Voight@gmail.com>
2015-06-11 13:59:24 -0400

    second commit

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'pry'
 
 require_relative 'models/contact'
 also_reload 'models/contact'
@@ -11,6 +12,7 @@ get '/' do
 end
 
 get '/:id' do
-  @contacts = Contact.all
+  first_name = params[:id].split(" ").first
+  @contacts = Contact.find_by first_name: first_name
   erb :show, locals: { id: params[:id] }
 end
